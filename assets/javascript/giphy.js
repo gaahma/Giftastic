84e4e105d567529131535c59818d99f8b4b5e9c6
Adam Haag <adam.m.h@gmail.com>
2017-07-08 12:25:00 -0400

    final touches

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -17,12 +17,7 @@ var giphy = {
 
 	addGifs: function(gifs){
 		$("#gifs").empty();
-		if (gifs.length >= 10){
-			var number = 10;
-		} else {
-			var number = gifs.length;
-		}
-		for(var i = 0; i < number; i++){
+		for(var i = 0; i < gifs.length; i++){
 			var still = gifs[i].images.fixed_height_still.url;
 			var animated = gifs[i].images.fixed_height.url;
 			var gifDiv = $("<div>")
@@ -43,6 +38,11 @@ var giphy = {
 $(document).ready(function(){
 	giphy.init();
 
+/*
+	This function takes the input from the #gif-name text box, 
+	and creates a new button with the input.  Then it appends
+	to #buttons and empties #gif-name
+*/
 	$("#add-gif").on("click", function(){
 		event.preventDefault();
 		var newTopic = $("#gif-name").val().trim();
@@ -53,10 +53,15 @@ $(document).ready(function(){
 		$("#gif-name").val("");
 
 	});
-
+/*
+	This click function takes the data-topic of the clicked button
+	and searches the giphy api for gifs.  The response is passed to
+	the addGifs function, which then puts the gifs on the browswer
+*/
 	$(document).on("click", "button", function(){
 		var gifData = {
 			"q": $(this).attr("data-topic"),
+			"limit": "10",
 			"rating": "pg",
 			"api_key": "50fcc85a481a4571beac4ac39a389a47"
 		}
@@ -69,6 +74,9 @@ $(document).ready(function(){
 		});
 	});
 
+/*
+	This click function turns an animated gif into a still gif, and vice versa
+*/
 	$(document).on("click", ".gif", function(){
 		if($(this).attr("data-state") === "still"){
 			$(this).attr("src", $(this).attr("data-animate"))
